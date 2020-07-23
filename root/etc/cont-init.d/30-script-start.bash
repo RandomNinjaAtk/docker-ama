b2ecd54e90422d16d6dc5bd7c0ba3b0549edbf46
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-07-23 09:02:59 -0400

    Update 30-script-start.bash

--- root/etc/cont-init.d/30-script-start.bash
+++ root/etc/cont-init.d/30-script-start.bash
@@ -2,7 +2,7 @@
 
 if [ "$AUTOSTART" = "true" ]; then
 	echo "Automatic Start Enabled, starting..."
-  bash /config/script/start.bash
+	bash /config/script/start.bash
 else
 	echo "Automatic Start Disabled, manually run using this command:"
 	echo "bash /config/script/start.bash"
