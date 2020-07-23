bc7f165ee4ad2a61942109c8b9b37494fe8f327f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-07-23 13:55:41 -0400

    updated

--- root/scripts/start.bash
+++ root/scripts/start.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/with-contenv bash
 
 echo "Starting Script...."
-processid="$(pgrep -f start.bash)"
+processstartid="$(pgrep -f /config/scripts/start.bash)"
 echo "To kill script, use the following command:"
-echo "kill -9 $processid"
+echo "kill -9 $processstartid"
 for (( ; ; )); do
     let i++
     bash /config/scripts/download.bash 2>&1 | tee "/config/logs/script_run_${i}_$(date +"%Y_%m_%d_%I_%M_%p").log" > /proc/1/fd/1 2>/proc/1/fd/2
