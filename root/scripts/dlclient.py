e8403ecf22690f06f27dcaa6dd512dfe0a782d51
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-09-04 14:11:50 -0400

    Bugfix for concurrency

--- root/scripts/dlclient.py
+++ root/scripts/dlclient.py
@@ -20,7 +20,7 @@ class cli(deemix):
         self.set.settings["createCDFolder"] = False
         self.set.settings["createAlbumFolder"] = False
         self.set.settings["saveArtworkArtist"] = True
-        self.set.settings["queueConcurrency"] = 6
+        self.set.settings["queueConcurrency"] = 1
         self.set.settings["jpegImageQuality"] = 90
         self.set.settings["embeddedArtworkSize"] = 1800
         self.set.settings["localArtworkSize"] = 1800
